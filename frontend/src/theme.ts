08d997a931853b7bc8e331bc625bdd53f1a8ff02
malvinh1 <malvinhariyanto50@gmail.com>
2020-12-14 12:36:48 +0700

    add typography

--- frontend/src/theme.ts
+++ frontend/src/theme.ts
@@ -1,17 +1,52 @@
-import {createMuiTheme} from '@material-ui/core';
+import { createMuiTheme } from '@material-ui/core';
 
 export const theme = createMuiTheme({
   palette: {
     primary: {
-      main: '#ff4400',
-      light: '#fff',
+      main: '#3E495B',
+      light: '#FFF',
     },
-    // secondary: {
-    //   main: '',
-    //   light: '',
-    //   dark: '',
-    // },
+    secondary: {
+      main: '#3E495B',
+      light: '#F4F8FE',
+    },
   },
-  spacing: 8,
-  // typography: {},
+  typography: {
+    fontFamily: 'manrope',
+    h1: {
+      fontSize: 36,
+      fontWeight: 'bold',
+      color: '#0E1C32',
+    },
+    h2: {
+      fontSize: 28,
+      fontWeight: 'bold',
+      color: '#0E1C32',
+    },
+    subtitle1: {
+      fontSize: 18,
+      fontWeight: 'bold',
+      color: '#0E1C32',
+    },
+    subtitle2: {
+      fontSize: 18,
+      fontWeight: 'normal',
+      color: '#0E1C32',
+    },
+    body1: {
+      fontSize: 16,
+      fontWeight: 'bold',
+      color: '#0E1C32',
+    },
+    body2: {
+      fontSize: 16,
+      fontWeight: 'normal',
+      color: '#3E495B',
+    },
+    caption: {
+      fontSize: 14,
+      fontWeight: 'normal',
+      color: '#3E495B',
+    },
+  },
 });
