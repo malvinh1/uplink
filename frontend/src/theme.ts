34da14ff083bdea748532890dd6a691a21b5b419
malvinh1 <malvinhariyanto50@gmail.com>
2020-12-15 11:50:59 +0700

    add responsive typography

--- frontend/src/theme.ts
+++ frontend/src/theme.ts
@@ -1,5 +1,8 @@
 import { createMuiTheme } from '@material-ui/core';
 
+const defaultTheme = createMuiTheme();
+const { breakpoints } = defaultTheme;
+
 export const theme = createMuiTheme({
   palette: {
     primary: {
@@ -17,36 +20,57 @@ export const theme = createMuiTheme({
       fontSize: 36,
       fontWeight: 'bold',
       color: '#0E1C32',
+      [breakpoints.down('xs')]: {
+        fontSize: 30,
+      },
     },
     h2: {
       fontSize: 28,
       fontWeight: 'bold',
       color: '#0E1C32',
+      [breakpoints.down('xs')]: {
+        fontSize: 22,
+      },
     },
     subtitle1: {
       fontSize: 18,
       fontWeight: 'bold',
       color: '#0E1C32',
+      [breakpoints.down('xs')]: {
+        fontSize: 14,
+      },
     },
     subtitle2: {
       fontSize: 18,
       fontWeight: 'normal',
       color: '#0E1C32',
+      [breakpoints.down('xs')]: {
+        fontSize: 14,
+      },
     },
     body1: {
       fontSize: 16,
       fontWeight: 'bold',
       color: '#0E1C32',
+      [breakpoints.down('xs')]: {
+        fontSize: 12,
+      },
     },
     body2: {
       fontSize: 16,
       fontWeight: 'normal',
       color: '#3E495B',
+      [breakpoints.down('xs')]: {
+        fontSize: 12,
+      },
     },
     caption: {
       fontSize: 14,
       fontWeight: 'normal',
       color: '#3E495B',
+      [breakpoints.down('xs')]: {
+        fontSize: 10,
+      },
     },
   },
 });
