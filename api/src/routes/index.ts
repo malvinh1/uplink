4a24feedbff0714320a05fa2b132781adca4871c
malvinh1 <malvinhariyanto50@gmail.com>
2020-12-23 11:37:12 +0700

    update api

--- api/src/routes/index.ts
+++ api/src/routes/index.ts
@@ -3,9 +3,5 @@ import express, { Request, Response } from 'express';
 export const router = express.Router();
 
 router.get('/', (_: Request, res: Response) => {
-  res.send('Hello world');
-});
-
-router.get('/user', (_, res: Response) => {
-  res.send('MASUK');
+  res.send('This is uplink API');
 });
